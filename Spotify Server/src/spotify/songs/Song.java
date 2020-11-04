6b8773d1b3b314162ec7d584c25566d82a16061e
nkyakimov <43946104+nkyakimov@users.noreply.github.com>
2020-11-04 19:17:40 +0200

    Add files via upload

--- Spotify Server/src/spotify/songs/Song.java
+++ Spotify Server/src/spotify/songs/Song.java
@@ -4,54 +4,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Song {
-    private final String name;
-    private final ArrayList<String> artists;
-    private final double length;
-    private final String location;
-    private final Integer id;
+  private final String name;
+  private final ArrayList<String> artists;
+  private final double length;
+  private final String location;
+  private final Integer id;
 
-    public Song(String[] songInformation) throws IndexOutOfBoundsException {
-        String[] artist = new String[songInformation.length - 4];
+  public Song(String[] songInformation) throws IndexOutOfBoundsException {
+    String[] artist = new String[songInformation.length - 4];
 
-        if (songInformation.length - 1 - 2 >= 0)
-            System.arraycopy(songInformation, 3, artist, 0, songInformation.length - 4);
-        id = Integer.parseInt(songInformation[0]);
-        name = songInformation[1];
-        artists = new ArrayList<>(Arrays.asList(artist));
-        length = Double.parseDouble(songInformation[2]);
-        location = songInformation[songInformation.length - 1];
-    }
+    if (songInformation.length - 1 - 2 >= 0)
+      System.arraycopy(songInformation, 3, artist, 0, songInformation.length - 4);
+    id = Integer.parseInt(songInformation[0]);
+    name = songInformation[1];
+    artists = new ArrayList<>(Arrays.asList(artist));
+    length = Double.parseDouble(songInformation[2]);
+    location = songInformation[songInformation.length - 1];
+  }
 
-    public String getLocation() {
-        return location;
-    }
+  public String getLocation() {
+    return location;
+  }
 
-    public boolean match(String info) {
-        return Arrays.stream(info.replaceAll("( +)", " ").trim().split(" ")).allMatch(
-                data -> getArtists().stream().anyMatch(artist -> artist.toLowerCase().contains(data.toLowerCase())) ||
-                        getName().toLowerCase().contains(data.toLowerCase())
-        );
-    }
+  public boolean match(String info) {
+    return Arrays.stream(info.replaceAll("( +)", " ").trim().split(" "))
+        .allMatch(
+            data ->
+                getArtists().stream()
+                        .anyMatch(artist -> artist.toLowerCase().contains(data.toLowerCase()))
+                    || getName().toLowerCase().contains(data.toLowerCase()));
+  }
 
-    public Integer getId() {
-        return id;
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public ArrayList<String> getArtists() {
-        return artists;
-    }
+  public ArrayList<String> getArtists() {
+    return artists;
+  }
 
-    public String getLengthString() {
-        return String.format("%.2f", length).replace(",",":");
-    }
+  public String getLengthString() {
+    return String.format("%.2f", length).replace(",", ":");
+  }
 
-    public double getLengthDouble() {return length;}
+  public double getLengthDouble() {
+    return length;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String toString() {
-        return name + "\t" + getLengthString() + "\t" + getArtists();// + "  location: "+location;
-    }
+  public String toString() {
+    return name + "\t" + getLengthString() + "\t" + getArtists(); // + "  location: "+location;
+  }
 }
--- Spotify Server/src/spotify/songs/SongDataBase.java
+++ Spotify Server/src/spotify/songs/SongDataBase.java
@@ -3,46 +3,58 @@ package spotify.songs;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class SongDataBase {
-    private final HashMap<Integer, Song> songs;
-    private final String dbSongsLocation;
+  private final HashMap<Integer, Song> songs;
+  private final String dbSongsLocation;
 
-    public SongDataBase(String dbSongsLocation) {
-        this.dbSongsLocation = dbSongsLocation;
-        songs = new HashMap<>();
-        load();
-    }
+  public SongDataBase(String dbSongsLocation) {
+    this.dbSongsLocation = dbSongsLocation;
+    songs = new HashMap<>();
+    loadSongDataBase();
+  }
 
-    public Song getSong(final Integer id) {
-        return songs.get(id);
-    }
+  public Song getSong(final Integer id) {
+    return songs.get(id);
+  }
 
-    public Song[] getSongs(final String nameOrArtist) {
-        return songs.values().stream().filter(song -> song.match(nameOrArtist)).toArray(Song[]::new);
-    }
+  public Song[] getSongs(final String nameOrArtist) {
+    return songs.values().stream().filter(song -> song.match(nameOrArtist)).toArray(Song[]::new);
+  }
 
-    private void load() {
-        try (Scanner scanner = new Scanner(new File(dbSongsLocation))) {
-            while (scanner.hasNextLine()) {
-                Arrays.stream(scanner.nextLine().split(";")).forEach(i -> {
-                    String[] data = i.split(",");
-                    if (songs.put(Integer.parseInt(data[0]), new Song(Arrays.stream(data).map(String::trim).toArray(String[]::new))) != null) {
-                        songs.clear();
-                        System.out.println("Error in id");
-                        throw new IllegalArgumentException();
-                    }
+  private void loadSongDataBase() {
+    try (Scanner scanner = new Scanner(new File(dbSongsLocation))) {
+      while (scanner.hasNextLine()) {
+        Arrays.stream(scanner.nextLine().split(";"))
+            .forEach(
+                i -> {
+                  String[] data = i.split(",");
+                  if (songs.put(
+                          Integer.parseInt(data[0]),
+                          new Song(Arrays.stream(data).map(String::trim).toArray(String[]::new)))
+                      != null) {
+                    songs.clear();
+                    System.out.println("Error in id");
+                    throw new IllegalArgumentException();
+                  }
                 });
-            }
-        } catch (FileNotFoundException e) {
-            File file = new File(dbSongsLocation);
-            try {
-                if (!file.createNewFile())
-                    throw new IOException();
-            } catch (IOException ioException) {
-                System.err.println("Cant create sdb file");
-            }
-        }
+      }
+    } catch (FileNotFoundException e) {
+      createSongDataBase();
     }
+  }
+
+  private void createSongDataBase() {
+    File file = new File(dbSongsLocation);
+    try {
+      if (!file.createNewFile()) {
+        throw new IOException();
+      }
+    } catch (IOException ioException) {
+      System.err.println("Cant create sdb file");
+    }
+  }
 }
