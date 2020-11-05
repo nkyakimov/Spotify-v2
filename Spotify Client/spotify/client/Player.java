f8adff441251ddb31c9f03b5b097d6517667c045
Nikola Yakimov <43946104+nkyakimov@users.noreply.github.com>
2020-11-05 23:17:56 +0200

    Add files via upload

--- Spotify Client/spotify/client/Player.java
+++ Spotify Client/spotify/client/Player.java
@@ -20,6 +20,17 @@ public class Player implements Runnable {
     stopped = true;
   }
 
+  private AudioFormat.Encoding toEncoding(int i) {
+    return switch (i) {
+      case 1 -> AudioFormat.Encoding.ALAW;
+      case 2 -> AudioFormat.Encoding.PCM_FLOAT;
+      case 3 -> AudioFormat.Encoding.PCM_SIGNED;
+      case 4 -> AudioFormat.Encoding.PCM_UNSIGNED;
+      case 5 -> AudioFormat.Encoding.ULAW;
+      default -> null;
+    };
+  }
+
   @Override
   public void run() {
     InputStream is = null;
@@ -30,31 +41,32 @@ public class Player implements Runnable {
       br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       AudioFormat format =
           new AudioFormat(
-              AudioFormat.Encoding.PCM_SIGNED,
+              toEncoding(Integer.parseInt(br.readLine())),
               Float.parseFloat(br.readLine()),
               Integer.parseInt(br.readLine()),
               Integer.parseInt(br.readLine()),
               Integer.parseInt(br.readLine()),
               Float.parseFloat(br.readLine()),
               Boolean.parseBoolean(br.readLine()));
-      SourceDataLine.Info info = new DataLine.Info(SourceDataLine.class, format, 2048);
+      SourceDataLine.Info info = new DataLine.Info(SourceDataLine.class, format, 512);
       SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
       line.open(format);
       line.start();
 
       int numRead;
-      byte[] buff = new byte[2048];
+      byte[] buff = new byte[512];
       while ((numRead = is.read(buff)) >= 0 && !stopped) {
         line.write(buff, 0, numRead);
       }
       line.drain();
       line.stop();
-      System.out.println("Stopped playing");
+
     } catch (IOException | NullPointerException | LineUnavailableException ignored) {
 
     } catch (IllegalArgumentException e) {
       System.err.println("Something is wrong");
     } finally {
+      System.out.println("Stopped playing");
       try {
         socket.shutdownInput();
         if (is != null) is.close();
