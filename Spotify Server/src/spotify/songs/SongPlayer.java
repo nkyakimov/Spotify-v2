181853d8015386e912817e2708fa02fdd9224efb
Nikola Yakimov <43946104+nkyakimov@users.noreply.github.com>
2020-11-05 23:17:27 +0200

    Add files via upload

--- Spotify Server/src/spotify/songs/SongPlayer.java
+++ Spotify Server/src/spotify/songs/SongPlayer.java
@@ -21,6 +21,44 @@ public class SongPlayer implements Runnable {
     stopped = true;
   }
 
+  private int toInt(AudioFormat.Encoding e) {
+    if (AudioFormat.Encoding.ALAW.equals(e)) {
+      return 1;
+    } else if (AudioFormat.Encoding.PCM_FLOAT.equals(e)) {
+      return 2;
+    } else if (AudioFormat.Encoding.PCM_SIGNED.equals(e)) {
+      return 3;
+    } else if (AudioFormat.Encoding.PCM_UNSIGNED.equals(e)) {
+      return 4;
+    } else if (AudioFormat.Encoding.ULAW.equals(e)) {
+      return 5;
+    }
+    return -1;
+  }
+
+  private AudioInputStream getStream() {
+    try {
+      PrintWriter pr = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+      AudioInputStream stream = AudioSystem.getAudioInputStream(new File(song.getLocation()));
+
+      AudioFormat format = stream.getFormat();
+
+      pr.println(toInt(format.getEncoding()));
+      pr.println(format.getSampleRate());
+      pr.println(format.getSampleSizeInBits());
+      pr.println(format.getChannels());
+      pr.println(format.getFrameSize());
+      pr.println(format.getFrameRate());
+      pr.println(format.isBigEndian());
+      return stream;
+    } catch (IOException | NullPointerException e) {
+      System.err.println("File not found");
+    } catch (UnsupportedAudioFileException e) {
+      System.err.println("File type not .wav");
+    }
+    return null;
+  }
+
   @Override
   public void run() {
 
@@ -30,37 +68,16 @@ public class SongPlayer implements Runnable {
         return;
       }
       osw = socket.getOutputStream();
-      PrintWriter pr = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-      AudioInputStream stream = AudioSystem.getAudioInputStream(new File(song.getLocation()));
-      AudioFormat format = stream.getFormat();
-      System.out.println(format.getEncoding());
-      if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
-        format =
-            new AudioFormat(
-                AudioFormat.Encoding.PCM_SIGNED,
-                format.getSampleRate(),
-                format.getSampleSizeInBits() * 2,
-                format.getChannels(),
-                format.getFrameSize() * 2,
-                format.getFrameRate(),
-                true);
-        stream = AudioSystem.getAudioInputStream(format, stream);
+      AudioInputStream stream = getStream();
+      if (stream == null) {
+        throw new IOException();
       }
-      pr.println(format.getSampleRate());
-      pr.println(format.getSampleSizeInBits());
-      pr.println(format.getChannels());
-      pr.println(format.getFrameSize());
-      pr.println(format.getFrameRate());
-      pr.println(format.isBigEndian());
-
       int numRead;
       int size = 8192;
       byte[] buff = new byte[size];
       while ((numRead = stream.read(buff)) >= 0 && !stopped) {
         osw.write(buff, 0, numRead);
       }
-    } catch (UnsupportedAudioFileException e) {
-      System.err.println("File type not supported");
     } catch (IOException ignored) {
 
     } finally {
