25eb537eeffe770e250d4db5d7cbb381cced836d
Nikola Yakimov <43946104+nkyakimov@users.noreply.github.com>
2020-11-06 19:34:38 +0200

    Add files via upload

--- Spotify Server/src/spotify/server/SpotifyServer.java
+++ Spotify Server/src/spotify/server/SpotifyServer.java
@@ -2,12 +2,18 @@ package spotify.server;
 
 import spotify.dataBase.AccountDataBase;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SpotifyServer {
   private final AccountDataBase program;
+
+  private void addSong(String info) {
+    program.addSong(info);
+  }
 
   public SpotifyServer(String adb, String sdb, String acf) {
     this.program = new AccountDataBase(adb, sdb, acf);
@@ -18,7 +24,47 @@ public class SpotifyServer {
     SpotifyServer spotify =
         new SpotifyServer(
             src + "/accounts/", src + "/songs/songs.sg", src + "/songs/songCounter.sg");
+    Thread listener =
+        new Thread(
+            () -> {
+              BufferedReader admin = new BufferedReader(new InputStreamReader(System.in));
+              while (true) {
+                String command;
+                try {
+                  command = admin.readLine();
+                } catch (IOException e) {
+                  continue;
+                }
+                if (command.startsWith("add")) {
+                  spotify.addSong(command.substring(4));
+                } else if (command.startsWith("?")) {
+                  System.out.println("add <id>,<name>,<length>,<artist>...,<location>");
+                  System.out.println("remove <id>");
+                  System.out.println(
+                      "!!! The Song DataBase wont check if this song already exists !!!");
+                  spotify.update();
+                } else if (command.startsWith("remove")) {
+                  spotify.removeSong(Integer.parseInt(command.substring(7)));
+                  spotify.update();
+                } else if (command.equals("print")) {
+                  spotify.print();
+                }
+              }
+            });
+    listener.start();
     spotify.launch();
+  }
+
+  private void update() {
+    program.update();
+  }
+
+  private void print(){
+    program.printSDB();
+  }
+
+  private void removeSong(int parseInt) {
+    program.removeSong(parseInt);
   }
 
   private void launch() {
