15ccaffb38a9770fcf1820f3dcfd6fd3eabc8123
Nikola Yakimov <43946104+nkyakimov@users.noreply.github.com>
2020-11-19 18:35:20 +0200

    Add files via upload

--- Spotify Server/src/spotify/server/SpotifyClientHandler.java
+++ Spotify Server/src/spotify/server/SpotifyClientHandler.java
@@ -1,10 +1,10 @@
 package spotify.server;
 
 import spotify.dataBase.Account;
-import spotify.dataBase.AccountDataBase;
 import spotify.dataBase.Playlist;
+import spotify.dataBase.ProgramDataBase;
+import spotify.exceptions.AccountCreationWentWrong;
 import spotify.songs.Song;
-import spotify.songs.SongPlayer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,19 +12,37 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class SpotifyClientHandler implements Runnable {
-  private final AccountDataBase program;
+  private final ProgramDataBase program;
   private final Socket socket;
-  private final ServerSocket tempServerSocket;
+  private final int splSocket, songIdSocket;
+  private final ServerSocket songIDServerSocket;
   private Account currentAccount;
-  private SongPlayer spl;
+  private PrintWriter printWriter;
+  private BufferedReader bufferedReader;
 
-  public SpotifyClientHandler(AccountDataBase program, Socket socket, ServerSocket playingSocket) {
+  public SpotifyClientHandler(
+      ProgramDataBase program,
+      Socket socket,
+      int splSocket,
+      int songIdSocket,
+      ServerSocket songIDServerSocket) {
     this.program = program;
     this.socket = socket;
-    tempServerSocket = playingSocket;
+    this.songIdSocket = songIdSocket;
+    this.splSocket = splSocket;
+    this.songIDServerSocket = songIDServerSocket;
+    try {
+      printWriter = new PrintWriter(socket.getOutputStream(), true);
+      bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    } catch (IOException e) {
+      printWriter = null;
+      bufferedReader = null;
+    }
   }
 
   private Song chooseSong(String info, PrintWriter pr, BufferedReader bf) {
@@ -39,6 +57,7 @@ public class SpotifyClientHandler implements Runnable {
     for (Song i : songs) {
       pr.println("#" + (index++) + " " + i);
     }
+    pr.println("?");
     try {
       index = Integer.parseInt(bf.readLine());
       if (index < 1 || index > songs.length) {
@@ -50,153 +69,201 @@ public class SpotifyClientHandler implements Runnable {
     return songs[index - 1];
   }
 
-  private void printHelp(PrintWriter pr) {
-    pr.println("login <user> <password>");
-    pr.println("new user <user> <password>");
-    pr.println("delete user <user> <password>");
-    pr.println("create <playlist>");
-    pr.println("add <playlist> <song or artist>");
-    pr.println("remove <playlist> (<song or artist>)");
-    pr.println("play <song>");
-    pr.println("print (<playlist>)");
-    pr.println("quit");
+  private void printHelp() {
+    printWriter.println("login <user> <password>");
+    printWriter.println("new user <user> <password>");
+    printWriter.println("delete user <user> <password>");
+    printWriter.println("create <playlist>");
+    printWriter.println("add <playlist> <song or artist>");
+    printWriter.println("remove <playlist> (<song or artist>)");
+    printWriter.println("play <song>");
+    printWriter.println("print (<playlist>)");
+    printWriter.println("quit");
   }
 
   @Override
   public void run() {
     try {
-      PrintWriter pr = new PrintWriter(socket.getOutputStream(), true);
-      BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      pr.println("Welcome to Spotify v2");
-      pr.println("For help, type help");
+      // todo rewrite this function
+      printWriter.println(splSocket);
+      printWriter.println(songIdSocket);
+      printWriter.println("Welcome to Spotify v2");
+      printWriter.println("For help, type help");
       while (true) {
-        String request = bf.readLine().trim();
-        if (request.startsWith("login")) {
-          try {
-            currentAccount =
-                program.getAccount(request.split(" ")[1].trim(), request.split(" ")[2].trim());
-            if (currentAccount == null) throw new Exception();
-            System.out.println("Hello " + request.split(" ")[1]);
-            pr.println("Hello " + request.split(" ")[1]);
-          } catch (Exception e) {
-            currentAccount = null;
-            System.out.println("Wrong account");
-            pr.println("Wrong account");
-          }
-        } else if (request.equals("help")) {
-          printHelp(pr);
-        } else if (request.startsWith("new user")) {
-          String[] data = request.split(" ");
-          if (program.addAccount(data[2], data[3])) pr.println("User added");
-          else pr.println("User already exists");
-        } else if (request.startsWith("delete user")) {
-          if (program.removeAccount(request.split(" ")[2], request.split(" ")[3])) {
-            pr.println("User removed");
-            currentAccount = null;
-            pr.println("Logged out");
-          } else {
-            pr.println("Wrong user");
-          }
-        } else if (request.startsWith("remove")) {
+        try {
+          String request = bufferedReader.readLine().trim();
+          if (request.startsWith("login")) {
+            login(request.substring(6));
+          } else if (request.equals("help")) {
+            printHelp();
+          } else if (request.startsWith("new user")) {
+            addUser(request.substring(8).trim());
+          } else if (request.startsWith("delete user")) {
+            if (program.removeAccount(request.split(" ")[2], request.split(" ")[3])) {
+              printWriter.println("User removed");
+              currentAccount = null;
+              printWriter.println("Logged out");
+            } else {
+              printWriter.println("Wrong user");
+            }
+          } else if (request.startsWith("remove")) {
 
-          if (request.split(" ").length == 2) {
-            currentAccount.removePlaylist(currentAccount.getPlayList(request.substring(6).trim()));
-            pr.println("Playlist removed");
-            continue;
-          }
-          int space = request.indexOf(" ", request.indexOf(" ") + 1);
-          Playlist playlist = currentAccount.getPlayList(request.substring(6, space).trim());
-          try {
-            if (playlist.remove(
-                chooseSong(request.substring(request.indexOf(" ", space)).trim(), pr, bf))) {
-              pr.println("Song removed");
-            } else {
-              throw new NullPointerException();
+            if (request.split(" ").length == 2) {
+              currentAccount.removePlaylist(
+                  currentAccount.getPlayList(request.substring(6).trim()));
+              printWriter.println("Playlist removed");
+              continue;
             }
-          } catch (NullPointerException e) {
-            pr.println("Cannot remove this song");
-          }
+            int space = request.indexOf(" ", request.indexOf(" ") + 1);
+            Playlist playlist = currentAccount.getPlayList(request.substring(6, space).trim());
+            try {
+              if (playlist.remove(
+                  chooseSong(
+                      request.substring(request.indexOf(" ", space)).trim(),
+                      printWriter,
+                      bufferedReader))) {
+                printWriter.println("Song removed");
+              } else {
+                throw new NullPointerException();
+              }
+            } catch (NullPointerException e) {
+              printWriter.println("Cannot remove this song");
+            }
 
-        } else if (request.startsWith("play")) {
-          Song choice = chooseSong(request.substring(request.indexOf(" ") + 1).trim(), pr, bf);
-          Socket tempSocket = tempServerSocket.accept();
-          if (choice != null) {
-            spl = new SongPlayer(choice, tempSocket);
-            program.listen(choice);
-            Thread splThread = new Thread(spl);
-            splThread.start();
-            pr.println(
-                "Now playing:  "
-                    + choice.getName()
-                    + " by "
-                    + Arrays.toString(choice.getArtists().toArray()));
+          } else if (request.startsWith("play")) {
+            Song choice =
+                chooseSong(
+                    request.substring(request.indexOf(" ") + 1).trim(),
+                    printWriter,
+                    bufferedReader);
+            if (choice != null) {
+              program.listen(choice);
+              printWriter.println(
+                  "Now playing:  "
+                      + choice.getName()
+                      + " by "
+                      + Arrays.toString(choice.getArtists().toArray()));
+              try {
+                Socket temp = songIDServerSocket.accept();
+                PrintWriter tempPr = new PrintWriter(temp.getOutputStream(), true);
+                tempPr.println(choice.getId());
+                temp.close();
+              } catch (Exception ignored) {
+
+              }
+            } else {
+              try {
+                Socket temp = songIDServerSocket.accept();
+                PrintWriter tempPr = new PrintWriter(temp.getOutputStream(), true);
+                tempPr.println(-1);
+                temp.close();
+              } catch (Exception ignored) {
+
+              }
+              printWriter.println("No such song");
+            }
+          } else if (request.equals("print")) {
+            try {
+              currentAccount.print(printWriter);
+            } catch (NullPointerException e) {
+              printWriter.println("You are not logged in");
+            }
+          } else if (request.startsWith("print ")) {
+            try {
+              currentAccount.getPlayList(request.split("print")[1].trim()).print(printWriter);
+            } catch (Exception e) {
+              printWriter.println("Something went wrong");
+            }
+          } else if (request.startsWith("logout")) {
+            currentAccount = null;
+            printWriter.println("Logged out");
+          } else if (request.startsWith("create")) {
+            try {
+              if (currentAccount.newPlaylist(request.substring(7).trim())) {
+                printWriter.println("Playlist added");
+              } else {
+                throw new NullPointerException();
+              }
+            } catch (NullPointerException e) {
+              printWriter.println("Something went wrong");
+            }
+          } else if (request.startsWith("top")) {
+            int max;
+            try {
+              max = Integer.parseInt(request.substring(3).trim());
+            } catch (NumberFormatException e) {
+              printWriter.println("Not a valid number");
+              continue;
+            }
+            program.top(max, printWriter);
+          } else if (request.startsWith("add")) {
+            if (currentAccount == null) {
+              printWriter.println("You are not logged in");
+            }
+            String[] info = request.split(" ");
+            Playlist playlist;
+            if ((playlist = currentAccount.getPlayList(info[1])) == null) {
+              printWriter.println("No such playlist, request aborted");
+              continue;
+            }
+            try {
+              if (playlist.addSong(
+                  chooseSong(
+                      Arrays.stream(info).skip(2).collect(Collectors.joining(" ")),
+                      printWriter,
+                      bufferedReader))) {
+                printWriter.println("Song added");
+              } else {
+                throw new NullPointerException();
+              }
+            } catch (NullPointerException e) {
+              printWriter.println("Song not added");
+            }
+          } else if (request.equals("quit")) {
+            break;
           } else {
-            tempSocket.close();
-            pr.println("No such song");
+            printWriter.println("Command not supported");
           }
-        } else if (request.equals("stop") && spl != null) {
-          spl.stop();
-        } else if (request.equals("print")) {
-          try {
-            currentAccount.print(pr);
-          } catch (NullPointerException e) {
-            pr.println("You are not logged in");
-          }
-        } else if (request.startsWith("print ")) {
-          try {
-            currentAccount.getPlayList(request.split("print")[1].trim()).print(pr);
-          } catch (Exception e) {
-            pr.println("Something went wrong");
-          }
-        } else if (request.startsWith("logout")) {
-          currentAccount = null;
-          pr.println("Logged out");
-        } else if (request.startsWith("create")) {
-          try {
-            if (currentAccount.newPlaylist(request.substring(7).trim())) {
-              pr.println("Playlist added");
-            } else {
-              throw new NullPointerException();
-            }
-          } catch (NullPointerException e) {
-            pr.println("Something went wrong");
-          }
-        } else if (request.startsWith("top")) {
-          int max;
-          try {
-            max = Integer.parseInt(request.substring(3).trim());
-          } catch (NumberFormatException e) {
-            pr.println("Not a valid number");
-            continue;
-          }
-          program.top(max, pr);
-        } else if (request.startsWith("add")) {
-          String[] info = request.split(" ");
-          Playlist playlist;
-          if ((playlist = currentAccount.getPlayList(info[1])) == null) {
-            pr.println("No such playlist, request aborted");
-            continue;
-          }
-          try {
-            if (playlist.addSong(chooseSong(info[2], pr, bf))) {
-              pr.println("Song added");
-            } else {
-              throw new NullPointerException();
-            }
-          } catch (NullPointerException e) {
-            pr.println("Song not added");
-          }
-        } else if (request.equals("quit")) {
-          break;
-        } else {
-          pr.println("Command not supported");
+        } catch (IndexOutOfBoundsException e) {
+          printWriter.println("Incorrect input");
         }
       }
     } catch (Exception ignored) {
-
     } finally {
       program.update();
+      try {
+        socket.close();
+      } catch (IOException ignored) {
+
+      }
       System.out.println("Client left");
+    }
+  }
+
+  private void addUser(String request) {
+    String[] data = request.split(" +");
+    try {
+      program.addAccount(data[0], data[1]);
+      printWriter.println("User added");
+    } catch (FileAlreadyExistsException e) {
+      printWriter.println("User already exists");
+    } catch (AccountCreationWentWrong e) {
+      System.err.println("Something is wrong with account creation");
+      printWriter.println("There is a temporary problem!");
+    }
+  }
+
+  private void login(String request) {
+    try {
+      currentAccount =
+          program.getAccount(request.trim().split(" +")[0], request.split(" ")[1].trim());
+      if (currentAccount == null) throw new Exception();
+      System.out.println("Hello " + request.split(" +")[0]);
+      printWriter.println("Hello " + request.split(" +")[0]);
+    } catch (Exception e) {
+      currentAccount = null;
+      System.out.println("Wrong account");
+      printWriter.println("Wrong account");
     }
   }
 }
--- Spotify Server/src/spotify/server/SpotifyServer.java
+++ Spotify Server/src/spotify/server/SpotifyServer.java
@@ -1,6 +1,7 @@
 package spotify.server;
 
-import spotify.dataBase.AccountDataBase;
+import spotify.dataBase.ProgramDataBase;
+import spotify.songs.SongPlayer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,58 +10,46 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SpotifyServer {
-  private final AccountDataBase program;
+  private final ProgramDataBase program;
+  private final int playerSocket, songIDSocket;
+  private ServerSocket clientServerSocket;
+  private ServerSocket songIDServerSocket;
+  private ServerSocket playerServerSocket;
+  private boolean running = true;
 
   private void addSong(String info) {
     program.addSong(info);
   }
 
-  public SpotifyServer(String adb, String sdb, String acf) {
-    this.program = new AccountDataBase(adb, sdb, acf);
+  public SpotifyServer(
+      String adb, String sdb, String acf, int clientSocket, int playerSocket, int songIDSocket) {
+    this.program = new ProgramDataBase(adb, sdb, acf);
+    this.playerSocket = playerSocket;
+    this.songIDSocket = songIDSocket;
+    try {
+      this.clientServerSocket = new ServerSocket(clientSocket);
+      this.playerServerSocket = new ServerSocket(playerSocket);
+      this.songIDServerSocket = new ServerSocket(songIDSocket);
+    } catch (IOException e) {
+      System.err.println("Cannot start client or player Server Socket");
+      clientServerSocket = null;
+      playerServerSocket = null;
+      songIDServerSocket = null;
+      running = false;
+    }
   }
 
-  public static void main(String[] args) {
-    String src = System.getProperty("user.dir");
-    SpotifyServer spotify =
-        new SpotifyServer(
-            src + "/accounts/", src + "/songs/songs.sg", src + "/songs/songCounter.sg");
-    Thread listener =
-        new Thread(
-            () -> {
-              BufferedReader admin = new BufferedReader(new InputStreamReader(System.in));
-              System.out.println("Type ? for help");
-              while (true) {
-                String command;
-                try {
-                  command = admin.readLine();
-                } catch (IOException e) {
-                  continue;
-                }
-                if (command.startsWith("add")) {
-                  spotify.addSong(command.substring(4));
-                } else if (command.startsWith("?")) {
-                  System.out.println("add <id>,<name>,<length>,<artist>...,<location>");
-                  System.out.println("remove <id>");
-                  System.out.println(
-                      "!!! The Song DataBase wont check if this song already exists !!!");
-                  spotify.update();
-                } else if (command.startsWith("remove")) {
-                  spotify.removeSong(Integer.parseInt(command.substring(7)));
-                  spotify.update();
-                } else if (command.equals("print")) {
-                  spotify.print();
-                }
-              }
-            });
-    listener.start();
-    spotify.launch();
+  private void help() {
+    System.out.println("add <id>,<name>,<length>,<artist>...,<location>");
+    System.out.println("remove <id>");
+    System.out.println("print");
   }
 
   private void update() {
     program.update();
   }
 
-  private void print(){
+  private void print() {
     program.printSDB();
   }
 
@@ -70,16 +59,103 @@ public class SpotifyServer {
 
   private void launch() {
     System.out.println("Starting Spotify v2");
-    try (ServerSocket server = new ServerSocket(8189);
-        ServerSocket playingSocket = new ServerSocket(8152)) {
-      while (true) {
-        Socket socket = server.accept();
-        SpotifyClientHandler a = new SpotifyClientHandler(program, socket, playingSocket);
-        Thread t = new Thread(a);
-        t.start();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    launchClientListener();
+    launchSongPlayerListener();
+    launchServerMaintenance();
+  }
+
+  private void launchServerMaintenance() {
+    Thread listener =
+        new Thread(
+            () -> {
+              BufferedReader admin = new BufferedReader(new InputStreamReader(System.in));
+              System.out.println("Type ? for help");
+              while (running) {
+                String command;
+                try {
+                  command = admin.readLine();
+                } catch (IOException e) {
+                  continue;
+                }
+                if (command.startsWith("add")) {
+                  addSong(command.substring(4));
+                  update();
+                } else if (command.startsWith("?")) {
+                  help();
+                } else if (command.startsWith("remove")) {
+                  removeSong(Integer.parseInt(command.substring(7)));
+                  update();
+                } else if (command.equals("print")) {
+                  print();
+                } else if (command.equals("quit")) {
+                  update();
+                  System.out.println("Goodbye");
+                  running = false;
+                  try {
+                    playerServerSocket.close();
+                  } catch (IOException ignored) {}
+                  try {
+                    songIDServerSocket.close();
+                  } catch (IOException ignored) {}
+                  try {
+                    clientServerSocket.close();
+                  } catch (IOException ignored) {}
+                }
+              }
+            });
+    listener.start();
+  }
+
+  private void launchSongPlayerListener() {
+    Thread songPlayerListener =
+        new Thread(
+            () -> {
+              try {
+                while (running) {
+                  Socket socket = playerServerSocket.accept();
+                  BufferedReader bf =
+                      new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                  int id = Integer.parseInt(bf.readLine());
+                  Thread spl = new Thread(new SongPlayer(program.getSong(id), socket));
+                  spl.start();
+                }
+              } catch (IOException e) {
+                System.err.println("Something went wrong when creating song player");
+              }
+            });
+    songPlayerListener.start();
+  }
+
+  private void launchClientListener() {
+    Thread clientListener =
+        new Thread(
+            () -> {
+              try {
+                while (running) {
+                  Socket socket = clientServerSocket.accept();
+                  SpotifyClientHandler clientHandler =
+                      new SpotifyClientHandler(
+                          program, socket, playerSocket, songIDSocket, songIDServerSocket);
+                  Thread clientHandlerThread = new Thread(clientHandler);
+                  clientHandlerThread.start();
+                }
+              } catch (IOException e) {
+                System.err.println("Something went wrong when creating starting client handler");
+              }
+            });
+    clientListener.start();
+  }
+
+  public static void main(String[] args) {
+    String src = System.getProperty("user.dir");
+    SpotifyServer spotify =
+        new SpotifyServer(
+            src + "/accounts/",
+            src + "/songs/songs.sg",
+            src + "/songs/songCounter.sg",
+            8189,
+            8152,
+            8153);
+    spotify.launch();
   }
 }
