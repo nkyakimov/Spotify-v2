422bdcd67c6cabc3ddc244cc29e9ab4930ee8718
Nikola Yakimov <43946104+nkyakimov@users.noreply.github.com>
2020-12-05 14:38:17 +0200

    Add files via upload

--- Spotify Server/src/spotify/songs/Song.java
+++ Spotify Server/src/spotify/songs/Song.java
@@ -1,80 +1,69 @@
 package spotify.songs;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Song implements Serializable {
-  private final String name;
-  private final ArrayList<String> artists;
-  private final double length;
-  private final String location;
-  private final Integer id;
+    private final Integer id;
+    private final String name;
+    private final double length;
+    private final List<String> artists;
+    private final String location;
+    public static final long serialVersionUID = -7413920028814202069L;
 
-  public Song(String[] songInformation) throws IndexOutOfBoundsException {
-    String[] artist = new String[songInformation.length - 4];
-
-    if (songInformation.length - 1 - 2 >= 0)
-      System.arraycopy(songInformation, 3, artist, 0, songInformation.length - 4);
-    id = Integer.parseInt(songInformation[0]);
-    name = songInformation[1];
-    double temp_time = 0;
-    try {
-      temp_time = Double.parseDouble(songInformation[2]);
-    } catch (NumberFormatException e) {
-      temp_time = 0;
-      System.err.println("Song time not written correctly (should be 3.45, not 3:45)");
-    } finally {
-      length = temp_time;
+    public Song(Integer id, String name, double length, List<String> artists, String location) {
+        this.id = id;
+        this.name = name;
+        this.length = length;
+        this.artists = artists;
+        this.location = location;
     }
-    artists = new ArrayList<>(Arrays.asList(artist));
-    location = songInformation[songInformation.length - 1];
-  }
 
-  public String getLocation() {
-    return location;
-  }
+    public String getLocation() {
+        return location;
+    }
 
-  public boolean match(String info) {
-    return Arrays.stream(info.split(" +"))
-        .allMatch(
-            data ->
-                getArtists().stream()
-                        .anyMatch(artist -> artist.toLowerCase().contains(data.toLowerCase()))
-                    || getName().toLowerCase().contains(data.toLowerCase()));
-  }
+    public boolean match(String info) {
+        return Arrays.stream(info.split(" +"))
+                .allMatch(
+                        data ->
+                                getArtists().stream()
+                                        .anyMatch(artist -> artist.toLowerCase().contains(data.toLowerCase()))
+                                        || getName().toLowerCase().contains(data.toLowerCase()));
+    }
 
-  public Integer getId() {
-    return id;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public ArrayList<String> getArtists() {
-    return artists;
-  }
+    public List<String> getArtists() {
+        return artists;
+    }
 
-  public String getLengthString() {
-    return String.format("%.2f", length).replace(",", ":");
-  }
+    public String getLengthString() {
+        return String.format("%.2f", length).replace(",", ":");
+    }
 
-  public double getLengthDouble() {
-    return length;
-  }
+    public double getLengthDouble() {
+        return length;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String toString() {
-    return name + "\t" + getLengthString() + "\t" + getArtists(); // + "  location: "+location;
-  }
+    public String toString() {
+        return name + "\t" + getLengthString() + "\t" + getArtists(); // + "  location: "+location;
+    }
 
-  public String[] toServer() {
-    return new String[] {
-      String.valueOf(id),
-      name,
-      getLengthString(),
-      Arrays.toString(artists.toArray(String[]::new)),
-      location
-    };
-  }
+    public String[] toServer() {
+        return new String[]{
+                String.valueOf(id),
+                name,
+                getLengthString(),
+                Arrays.toString(artists.toArray(String[]::new)),
+                location
+        };
+    }
 }
--- Spotify Server/src/spotify/songs/SongPlayer.java
+++ Spotify Server/src/spotify/songs/SongPlayer.java
@@ -6,89 +6,86 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class SongPlayer implements Runnable {
-  private final Song song;
-  private final Socket socket;
+    private final Song song;
+    private final Socket socket;
 
-  public SongPlayer(Song song, Socket socket) {
-    this.song = song;
-    this.socket = socket;
-  }
-
-  private int toInt(AudioFormat.Encoding e) {
-    if (AudioFormat.Encoding.ALAW.equals(e)) {
-      return 1;
-    } else if (AudioFormat.Encoding.PCM_FLOAT.equals(e)) {
-      return 2;
-    } else if (AudioFormat.Encoding.PCM_SIGNED.equals(e)) {
-      return 3;
-    } else if (AudioFormat.Encoding.PCM_UNSIGNED.equals(e)) {
-      return 4;
-    } else if (AudioFormat.Encoding.ULAW.equals(e)) {
-      return 5;
+    public SongPlayer(Song song, Socket socket) {
+        this.song = song;
+        this.socket = socket;
     }
-    return -1;
-  }
 
-  private AudioInputStream getStream() throws FileNotFoundException, UnsupportedAudioFileException {
-    try {
-      PrintWriter pr = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-      AudioInputStream stream = AudioSystem.getAudioInputStream(new File(song.getLocation()));
-
-      AudioFormat format = stream.getFormat();
-
-      pr.println(toInt(format.getEncoding()));
-      pr.println(format.getSampleRate());
-      pr.println(format.getSampleSizeInBits());
-      pr.println(format.getChannels());
-      pr.println(format.getFrameSize());
-      pr.println(format.getFrameRate());
-      pr.println(format.isBigEndian());
-      return stream;
-    } catch (IOException | NullPointerException e) {
-      throw new FileNotFoundException();
-    }
-  }
-
-  @Override
-  public void run() {
-
-    OutputStream osw = null;
-    try {
-      if (song == null) {
-        throw new PlaybackErrorException();
-      }
-      osw = socket.getOutputStream();
-      AudioInputStream stream;
-      stream = getStream();
-      int numRead;
-      int size = 8192;
-      byte[] buff = new byte[size];
-      while ((numRead = stream.read(buff)) >= 0) {
-        osw.write(buff, 0, numRead);
-      }
-    } catch (PlaybackErrorException e) {
-      System.err.println("Something went wrong in streaming of file");
-    } catch (UnsupportedAudioFileException e) {
-      System.err.println("Audio file type not supported. Please use .wav only");
-    } catch (FileNotFoundException e) {
-      System.err.println("Song file not found. Check " + song.getLocation());
-    } catch (IOException ignored) {
-
-    } finally {
-      try {
-        System.err.println("Playback stopped");
-        if (osw != null) {
-          osw.flush();
-          osw.close();
+    private int toInt(AudioFormat.Encoding e) {
+        if (AudioFormat.Encoding.ALAW.equals(e)) {
+            return 1;
+        } else if (AudioFormat.Encoding.PCM_FLOAT.equals(e)) {
+            return 2;
+        } else if (AudioFormat.Encoding.PCM_SIGNED.equals(e)) {
+            return 3;
+        } else if (AudioFormat.Encoding.PCM_UNSIGNED.equals(e)) {
+            return 4;
+        } else if (AudioFormat.Encoding.ULAW.equals(e)) {
+            return 5;
         }
-        socket.close();
-      } catch (IOException e) {
-        System.err.println("Error in closing song player outputs");
-      }
+        return -1;
     }
-  }
+
+    private AudioInputStream getStream() throws FileNotFoundException, UnsupportedAudioFileException {
+        try {
+            PrintWriter pr = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            AudioInputStream stream = AudioSystem.getAudioInputStream(new File(song.getLocation()));
+            AudioFormat format = stream.getFormat();
+            pr.println(toInt(format.getEncoding()));
+            pr.println(format.getSampleRate());
+            pr.println(format.getSampleSizeInBits());
+            pr.println(format.getChannels());
+            pr.println(format.getFrameSize());
+            pr.println(format.getFrameRate());
+            pr.println(format.isBigEndian());
+            return stream;
+        } catch (IOException | NullPointerException e) {
+            throw new FileNotFoundException();
+        }
+    }
+
+    @Override
+    public void run() {
+        try (OutputStream osw = socket.getOutputStream()) {
+            if (song == null) {
+                throw new PlaybackErrorException();
+            }
+            AudioInputStream stream = getStream();
+            int numRead;
+            int size = 8192;
+            byte[] buff = new byte[size];
+            while ((numRead = stream.read(buff)) >= 0) {
+                osw.write(buff, 0, numRead);
+            }
+        } catch (PlaybackErrorException e) {
+            e.printStackTrace();
+            System.err.println("Something went wrong in streaming of file");
+        } catch (UnsupportedAudioFileException e) {
+            System.err.println("Audio file type not supported. Please use .wav only");
+        } catch (FileNotFoundException e) {
+            System.err.println("Song file not found. Check " + song.getLocation());
+        } catch (IOException ignored) {
+
+        } finally {
+            try {
+                //System.out.println("Playback stopped");
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("Error in closing song player socket");
+            }
+        }
+    }
 }
