10a36bf8795cd8be90297676af2b46cd2ad52c89
Nikola Yakimov <43946104+nkyakimov@users.noreply.github.com>
2020-12-05 14:37:03 +0200

    Add files via upload

--- Spotify Server/src/spotify/exceptions/AccountCreationWentWrong.java
+++ Spotify Server/src/spotify/exceptions/AccountCreationWentWrong.java
@@ -4,4 +4,8 @@ public class AccountCreationWentWrong extends Throwable {
     public AccountCreationWentWrong() {
         super("Account creation went wrong");
     }
+
+    public AccountCreationWentWrong(String message) {
+        super(message);
+    }
 }
