03a7f6b8464832f1e654c24d36b4c7d053e3f8c9
Nikola Yakimov <43946104+nkyakimov@users.noreply.github.com>
2020-12-05 14:42:34 +0200

    Add files via upload

--- Spotify Client/spotify/client/Main.java
+++ Spotify Client/spotify/client/Main.java
@@ -6,83 +6,88 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Main {
-  private static Player player;
-  private static int splSocket, songId;
-  private static PrintWriter pr;
+    private static final String host = "localhost";
+    private static final int port = 8189;
+    private static Player player;
+    private static int splSocket;
+    private static int songId = -1;
 
-  public static void main(String[] args) {
-    try {
-      Socket socket = new Socket("localhost", 8189);
-      pr = new PrintWriter(socket.getOutputStream(), true);
-      BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-      System.out.print(">");
-      splSocket = Integer.parseInt(bf.readLine());
-      songId = Integer.parseInt(bf.readLine());
-      lauchConsoleOut(bf,pr,keyboard);
-      while (true) {
-        String code = keyboard.readLine();
-        pr.println(code);
-        if (code.equals("quit")) {
-          try {
-            player.stop();
-          } catch (NullPointerException ignored) {
-
-          }
-          pr.close();
-          bf.close();
-          socket.close();
-          break;
-        }
-        if (code.equals("stop")) {
-          player.stop();
-        }
-        if (code.startsWith("play")) {
-          try {
-            player.stop();
-            Thread.sleep(1500);
-          } catch (Exception ignored) {
-
-          }
-          try {
-            Socket temp = new Socket("localhost", songId);
-            BufferedReader idReader = new BufferedReader(new InputStreamReader(temp.getInputStream()));
-            int id = Integer.parseInt(idReader.readLine());
-            if (id != -1) {
-              player = new Player(splSocket, id);
-              Thread play = new Thread(player);
-              play.start();
-            }
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    } catch (Exception e) {
-      System.err.println("Something went wrong. Is the server on?");
+    public static void main(String[] args) {
+        runSpotifyClient();
     }
-  }
 
-  private static void lauchConsoleOut(BufferedReader bf,PrintWriter pw,BufferedReader keyboard) {
-    Thread sout =
-        new Thread(
-            () -> {
-              try {
+    private static void runSpotifyClient() {
+        new Thread(() -> {
+            try (Socket socket = new Socket(host, port);
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in))) {
+
+                System.out.print(">");
+                splSocket = Integer.parseInt(reader.readLine());
+                launchConsoleOut(reader);
                 while (true) {
-                  String result;
-                  System.out.println((result = bf.readLine()));
-                  if (result.equals("No such song") && player != null) {
-                    player.stop();
-                  }
-                  if(result.equals("?")) {
-                    pr.println(keyboard.readLine());
-                  }
+                    String code = keyboard.readLine();
+                    if (code.equals("quit")) {
+                        playerStop();
+                        writer.println(code);
+                        break;
+                    } else if (code.equals("stop")) {
+                        playerStop();
+                    } else {
+                        writer.println(code);
+                    }
                 }
-              } catch (Exception e) {
-                System.out.println("Connection terminated");
-                System.exit(0);
-              }
-            });
-    sout.start();
-  }
+            } catch (Exception e) {
+                System.err.println("Something went wrong. Is the server on?");
+            }
+        }).start();
+    }
+
+    private synchronized static void playerStop() {
+        try {
+            player.stop();
+            Thread.sleep(1000);
+            player = null;
+        } catch (Exception ignored) {
+
+        }
+    }
+
+
+    private static void launchConsoleOut(BufferedReader bf) {
+        new Thread(
+                () -> {
+                    try {
+                        while (true) {
+                            String result = bf.readLine();
+                            if (result.equals("No such song")) {
+                                playerStop();
+                            } else if (result.startsWith("songID ")) {
+                                getSongIdAndStart(result.substring(7));
+                                continue;
+                            }
+                            System.out.println(result);
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Connection terminated");
+                        System.exit(0);
+                    }
+                }).start();
+    }
+
+    private synchronized static void getSongIdAndStart(String id) {
+        try {
+            songId = Integer.parseInt(id.trim());
+            play();
+        } catch (NumberFormatException e) {
+            songId = -1;
+        }
+    }
+
+    private synchronized static void play() {
+        playerStop();
+        int temp = songId;
+        new Thread((player = new Player(host, splSocket, temp))).start();
+    }
 }
--- Spotify Client/spotify/client/Player.java
+++ Spotify Client/spotify/client/Player.java
@@ -1,81 +1,86 @@
 package spotify.client;
 
-import javax.sound.sampled.*;
-import java.io.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Player implements Runnable {
-  private final int socketAddress;
-  private boolean stopped = false;
-  private final int id;
-  private Socket socket;
+    private final int socketAddress;
+    private final String host;
+    private final int songID;
+    private boolean stopped = false;
 
-  public Player(int socketAddress,int id) {
-    this.socketAddress = socketAddress;
-    this.id = id;
-  }
-
-  public void stop() {
-    stopped = true;
-  }
-
-  private AudioFormat.Encoding toEncoding(int i) {
-    return switch (i) {
-      case 1 -> AudioFormat.Encoding.ALAW;
-      case 2 -> AudioFormat.Encoding.PCM_FLOAT;
-      case 3 -> AudioFormat.Encoding.PCM_SIGNED;
-      case 4 -> AudioFormat.Encoding.PCM_UNSIGNED;
-      case 5 -> AudioFormat.Encoding.ULAW;
-      default -> null;
-    };
-  }
-
-  @Override
-  public void run() {
-    InputStream is = null;
-    BufferedReader br = null;
-    try {
-      socket = new Socket("localhost", socketAddress);
-      is = socket.getInputStream();
-      PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
-      printWriter.println(id);
-      br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      AudioFormat format =
-          new AudioFormat(
-              toEncoding(Integer.parseInt(br.readLine())),
-              Float.parseFloat(br.readLine()),
-              Integer.parseInt(br.readLine()),
-              Integer.parseInt(br.readLine()),
-              Integer.parseInt(br.readLine()),
-              Float.parseFloat(br.readLine()),
-              Boolean.parseBoolean(br.readLine()));
-      SourceDataLine.Info info = new DataLine.Info(SourceDataLine.class, format, 512);
-      SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
-      line.open(format);
-      line.start();
-
-      int numRead;
-      byte[] buff = new byte[512];
-      while ((numRead = is.read(buff)) >= 0 && !stopped) {
-        line.write(buff, 0, numRead);
-      }
-      line.drain();
-      line.stop();
-
-    } catch (IOException | NullPointerException | LineUnavailableException ignored) {
-      ignored.printStackTrace();
-    } catch (IllegalArgumentException e) {
-      System.err.println("Something is wrong");
-    } finally {
-      System.out.println("Stopped playing");
-      try {
-        socket.shutdownInput();
-        if (is != null) is.close();
-        if (br != null) br.close();
-        socket.close();
-      } catch (Exception ignored) {
-
-      }
+    public Player(String host, int socketAddress, int songID) {
+        this.socketAddress = socketAddress;
+        this.host = host;
+        this.songID = songID;
     }
-  }
+
+    public void stop() {
+        stopped = true;
+    }
+
+    @Override
+    public void run() {
+        try (Socket socket = new Socket(host, socketAddress);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                InputStream is = socket.getInputStream();
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+            writer.println(songID);
+            AudioFormat format;
+            if ((format = getAudioFormat(reader)) == null) {
+                return;
+            }
+            SourceDataLine.Info info = new DataLine.Info(SourceDataLine.class, format, 512);
+            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();
+            //is.reset();
+            int numRead;
+            byte[] buff = new byte[512];
+            while ((numRead = is.read(buff)) >= 0 && !stopped) {
+                line.write(buff, 0, numRead);
+            }
+            line.drain();
+            line.stop();
+        } catch (IOException | LineUnavailableException e) {
+            System.err.println("Something is wrong");
+        } finally {
+            System.out.println("Stopped playing");
+        }
+    }
+
+    private AudioFormat getAudioFormat(BufferedReader reader) {
+        try {
+            return new AudioFormat(
+                    toEncoding(Integer.parseInt(reader.readLine())),
+                    Float.parseFloat(reader.readLine()),
+                    Integer.parseInt(reader.readLine()),
+                    Integer.parseInt(reader.readLine()),
+                    Integer.parseInt(reader.readLine()),
+                    Float.parseFloat(reader.readLine()),
+                    Boolean.parseBoolean(reader.readLine()));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private AudioFormat.Encoding toEncoding(int i) {
+        return switch (i) {
+            case 1 -> AudioFormat.Encoding.ALAW;
+            case 2 -> AudioFormat.Encoding.PCM_FLOAT;
+            case 3 -> AudioFormat.Encoding.PCM_SIGNED;
+            case 4 -> AudioFormat.Encoding.PCM_UNSIGNED;
+            case 5 -> AudioFormat.Encoding.ULAW;
+            default -> null;
+        };
+    }
 }
